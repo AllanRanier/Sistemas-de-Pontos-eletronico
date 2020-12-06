a53cf3185d25ce79ab492e3d6075c9bebb5e3e61
Allan Ranier <allanranier18@gmail.com>
2020-12-06 12:50:10 -0300

    commit

--- src/views/login.php
+++ src/views/login.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/comum.css">
-    <link rel="stylesheet" href="assets/css/login.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/icofont.min.css">
-    <title>In n' Out</title>
-</head>
-
-<body>
 
     <form class="form-login" action="#" method="post">
         <div class="login-card card">
             <div class="card-header">
-                <i class="icofont-travelling mr-2"></i>
-                <span class="font-weight-light">In</span>
-                <span class="font-weight-bold mx-2">N'</span>
-                <span class="font-weight-light">Out</span>
-                <i class="icofont-runner-alt-1 ml-2"></i>
+                
             </div>
             <div class="card-body">
                 <?php include(TEMPLATE_PATH . '/messages.php') ?>
@@ -44,7 +26,3 @@
             </div>
         </div>
     </form>
-
-</body>
-
-</html>
